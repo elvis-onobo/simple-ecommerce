6f4856dbeff030c8da91c0b8b558dd14fd61366c
elvisonobo <rapknowlogy@gmail.com>
2020-04-08 15:10:16 +0100

    added controllers

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+// wallet
+Route::get('/wallet', 'WalletController@index')->name('wallet');
+// product
+Route::get('/buy', 'ProductController@buy')->name('buy');
