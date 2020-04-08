e4cfcc216080078c96d67d4257906c2ae1711bfa
elvisonobo <rapknowlogy@gmail.com>
2020-04-08 18:11:12 +0100

    added /buy method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function buy($id){
+        $product = Product::where('id', $id)->first();
+
+        return view('pages.buy', compact('products'));
+    }
 }
