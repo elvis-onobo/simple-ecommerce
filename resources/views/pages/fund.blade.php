6b7ebaa1be8d0e89b4fd2708c34f95d5751d6b08
elvisonobo <rapknowlogy@gmail.com>
2020-04-08 21:34:10 +0100

    added fund pages

--- resources/views/pages/fund.blade.php
+++ resources/views/pages/fund.blade.php
@@ -3,19 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __('Fund Your Wallet') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="">
+                    <form method="POST" action="{{ route('fund-wallet') }}">
                         @csrf
 
                         <div class="form-group row">
                             <label for="amount" class="col-md-4 col-form-label text-md-right">{{ __('Amount') }}</label>
 
                             <div class="col-md-6">
-                                <input id="amount" type="amount" class="form-control @error('amount') is-invalid @enderror" name="amount" value="{{ old('amount') }}" required autocomplete="amount" autofocus>
+                                <input id="amount" type="number" class="form-control @error('amount') is-invalid @enderror rounded-0" name="amount" value="{{ old('amount') }}" step="100" required autocomplete="amount" autofocus>
 
                                 @error('amount')
                                     <span class="invalid-feedback" role="alert">
@@ -28,7 +28,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary rounded-0">
                                     {{ __('Fund') }}
                                 </button>
                             </div>
