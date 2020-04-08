8d118b2a30cad6293768f364bbe2fa1ffc157b9f
elvisonobo <rapknowlogy@gmail.com>
2020-04-08 18:09:28 +0100

    added products to index method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Product;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $products = Product::paginate(3);
+
+        return view('home', compact('products'));
     }
 }
