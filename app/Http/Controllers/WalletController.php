3517dba06ed5d6623dd0cc20f141e1319e4fb9f3
elvisonobo <rapknowlogy@gmail.com>
2020-04-08 21:33:36 +0100

    added fund_wallet method

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -13,4 +13,13 @@ class WalletController extends Controller
     public function fund(){
         return view('pages.fund');
     }
+
+    public function fund_wallet(Request $request){
+        session([
+            'amount' => $request->amount,
+            'user_id' => auth()->user()->id
+        ]);
+
+        return view('pages.deposit')
+    }
 }
