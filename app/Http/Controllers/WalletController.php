344191d1206ee3adfec24eed459ec6b553d58f46
elvisonobo <rapknowlogy@gmail.com>
2020-04-08 19:38:56 +0100

    added fund method

--- app/Http/Controllers/WalletController.php
+++ app/Http/Controllers/WalletController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class WalletController extends Controller
 {
-    //
+    public function index(){
+        return view('pages.wallet');
+    }
+
+    public function fund(){
+        return view('pages.fund');
+    }
 }
