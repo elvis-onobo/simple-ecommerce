eaaf874be2c0f42c24bd0dcb74fe0c5b7f24f909
elvisonobo <rapknowlogy@gmail.com>
2020-04-08 19:40:41 +0100

    modified Buy button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,21 +30,23 @@
     <!-- //Stats Board -->
 
     <div class="row justify-content-center">
-            @foreach($products as $product)
-            <div class="col-md-4 mb-2">
-                <div class="card">
+        @foreach($products as $product)
+        <div class="col-md-4 mb-2">
+            <div class="card">
+                <div class="card-body">
+                    <div>
+                        <img src="{{ $product->image }}" class="img-responsive" alt="{{ $product->name }}">
+                    </div>
                     <div class="card-body">
-                        <div>
-                            <img src="{{ $product->image }}" class="img-responsive" alt="{{ $product->name }}">
-                        </div>
-                        <p class="title">{{ $product->name }}</p>
-                        <p class="">{{ $product->price }}</p>
+                        <p class="title">{{ ucwords($product->name) }}</p>
+                        <p class="">&#8358;{{ number_format($product->price) }}</p>
                         <a href="{{ route('buy', [ 'id' => $product->id, 'slug'=>$product->slug]) }}" class="btn btn-primary rounded-0">Buy</a>
                     </div>
                 </div>
             </div>
-            @endforeach
-            {{ $products->links() }}
+        </div>
+        @endforeach
+        {{ $products->links() }}
 
     </div>
 </div>
