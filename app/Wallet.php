8569edaf3cedb3a847a9d10bfaae7785b7bbacbd
elvisonobo <rapknowlogy@gmail.com>
2020-04-09 12:48:04 +0100

    added fillables

--- app/Wallet.php
+++ app/Wallet.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wallet extends Model
 {
-    //
+    /*
+    * Mass assigned values
+    */
+    protected $fillable = [
+        'user_id', 'balance', 'reference', 'authorization', 'nature_of_tranx'
+    ];
 }
