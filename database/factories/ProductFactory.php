e3adae63fc137a0e0016294dd7f41f5275543b7c
elvisonobo <rapknowlogy@gmail.com>
2020-04-09 13:45:25 +0100

    modified to use numberBetween()

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,7 +9,7 @@ use App\Product;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
-        'price' => $faker->postcode,
+        'price' => $faker->numberBetween($min= 1000, $max= 20000),
         'description' => $faker->text,
         'image' => $faker->imageUrl($width = 300, $height = 150),
         'slug' => $faker->slug
