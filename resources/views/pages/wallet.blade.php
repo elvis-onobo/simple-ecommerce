526b47af277b57b20ecaa159e36269a1acd4db3b
elvisonobo <rapknowlogy@gmail.com>
2020-04-09 13:49:47 +0100

    minor modifictaions

--- resources/views/pages/wallet.blade.php
+++ resources/views/pages/wallet.blade.php
@@ -24,7 +24,7 @@
                     <p class="small text-center">Wallet Balance</p>
                     <a href="{{ route('enter-funds') }}" class="btn btn-primary rounded-0">Fund Wallet</a>
                     <a href="{{ route('gift') }}" class="btn btn-primary rounded-0">Gift Funds</a>
-                    <a href="" class="btn btn-primary rounded-0">History</a>
+                    <a href="{{ route('history') }}" class="btn btn-primary rounded-0">History</a>
                 </div>
             </div>
         </div>
