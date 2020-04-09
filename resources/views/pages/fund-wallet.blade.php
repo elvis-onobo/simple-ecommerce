43b831636a65a98bec47b1db33a8f71d94f56ce6
elvisonobo <rapknowlogy@gmail.com>
2020-04-09 09:12:46 +0100

    modified js position

--- resources/views/pages/fund-wallet.blade.php
+++ resources/views/pages/fund-wallet.blade.php
@@ -16,7 +16,6 @@
 
                     <div class="card border-0 rounded-0 m-1 col-md-12">
                         <div class="card-body">
-
                             <form>
                                 <script src="https://js.paystack.co/v1/inline.js"></script>
                                 <div id="paystackEmbedContainer"></div>
@@ -37,7 +36,7 @@
 
 <script>
 var amount = {!! json_encode(session('amount')*100, JSON_HEX_TAG)  !!};
-var user = {!! json_encode(auth()->user()->id, JSON_HEX_TAG)  !!};
+var user = {!! json_encode(md5(auth()->user()->id), JSON_HEX_TAG)  !!};
 var email = {!! json_encode(auth()->user()->email, JSON_HEX_TAG)  !!};
 var key = {!! json_encode(env('PAYSTACK_PUBLIC'), JSON_HEX_TAG)  !!};
 
@@ -46,8 +45,7 @@ PaystackPop.setup({
     email,
     amount,
     metadata: {
-        user,
-        amount
+        user
     },
 
     container: 'paystackEmbedContainer',
