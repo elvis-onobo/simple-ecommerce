639ed89bcc938536f0e6010cbbb47a801561e8d3
elvisonobo <rapknowlogy@gmail.com>
2020-04-10 21:40:43 +0100

    added auth and verified middleware

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 // wallet
-Route::get('/wallet', 'WalletController@index')->name('wallet');
-Route::get('/enter-funds', 'WalletController@enter_fund')->name('enter-funds');
-Route::post('/fund-wallet', 'WalletController@fund_wallet')->name('fund-wallet');
-Route::get('/fund-wallet', 'WalletController@fund_wallet')->name('fund-wallet');
-Route::get('/gift', 'WalletController@gift')->name('gift');
-Route::post('/gift', 'WalletController@gift_fund')->name('gift-fund');
-Route::get('/verify/{reference}', 'WalletController@verify')->name('verify');
-Route::get('/history', 'WalletController@history')->name('history');
-// product
-Route::get('/buy/{id}/{slug}', 'ProductController@buy')->name('buy');
-Route::get('/buy-from-wallet/{id}', 'ProductController@buy_from_wallet')->name('buy-from-wallet');
-Route::get('/card-buy/verify/{reference}', 'PurchaseController@verify')->name('verify-purchase');
-Route::get('/purchase-history', 'PurchaseController@history')->name('purchase-history');
+Route::middleware('verified')->group(function (){
+    Route::get('/wallet', 'WalletController@index')->name('wallet');
+    Route::get('/enter-funds', 'WalletController@enter_fund')->name('enter-funds');
+    Route::post('/fund-wallet', 'WalletController@fund_wallet')->name('fund-wallet');
+    Route::get('/fund-wallet', 'WalletController@fund_wallet')->name('fund-wallet');
+    Route::get('/gift', 'WalletController@gift')->name('gift');
+    Route::post('/gift', 'WalletController@gift_fund')->name('gift-fund');
+    Route::get('/verify/{reference}', 'WalletController@verify')->name('verify');
+    Route::get('/history', 'WalletController@history')->name('history');
+    // product
+    Route::get('/buy/{id}/{slug}', 'ProductController@buy')->name('buy');
+    Route::get('/buy-from-wallet/{id}', 'ProductController@buy_from_wallet')->name('buy-from-wallet');
+    Route::get('/card-buy/verify/{reference}', 'PurchaseController@verify')->name('verify-purchase');
+    Route::get('/purchase-history', 'PurchaseController@history')->name('purchase-history');
+});
