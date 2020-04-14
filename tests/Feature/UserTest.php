f048f2671935914d657cdeafa60864bcd01f347e
elvisonobo <rapknowlogy@gmail.com>
2020-04-14 22:27:27 +0100

    uncommented page compression and cache

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Product;
+use App\Wallet;
 use App\User;
 
 class UserTest extends TestCase
@@ -16,17 +17,29 @@ class UserTest extends TestCase
      *
      * @return void
      */
+    // welcome page
     public function test_that_a_user_can_see_welcome_page()
     {
         $response = $this->get('/');
 
         $response->assertSee("Elvis Onobo");
+        $response->assertSee("Visit Shop");
     }
 
+    // listings
+    public function test_that_user_can_see_public_listings(){
+        $response = $this->get('/listing');
+
+        $products = factory(Product::class)->create();
+
+        $response->assertOk();
+    }
+
+    // home
     public function test_that_a_logged_in_user_can_see_the_links_and_stats_and_products(){
 
         $user = factory(User::class)->create();
-        $product = factory(Product::class)->create();
+        $products = factory(Product::class)->create();
 
         $response = $this->actingAs($user)->get('home');
         $response->assertSee("Nothing");
@@ -35,10 +48,11 @@ class UserTest extends TestCase
         $response->assertSee("Buy");
     }
 
+    // buy
     public function test_that_a_user_is_sent_to_the_buy_page_onclick_buy_button(){
-        $this->withoutExceptionHandling();
+        // $this->withoutExceptionHandling();
         $user = factory(User::class)->create();
-        $product = factory(Product::class)->create();
+        $products = factory(Product::class)->create();
 
         $productData = Product::first();
 
@@ -49,8 +63,33 @@ class UserTest extends TestCase
             ]));
         $response->assertViewIs('pages.buy');
         $response->assertSeeInOrder(['Pay With Card', 'Pay From Wallet']);
-        // $response->assertViewHas('price');
     }
 
+    // purchase history
+    public function test_that_user_can_see_purchase_history(){
+        $this->withoutExceptionHandling();
 
+        $user = factory(User::class)->create();
+
+        $response = $this->actingAs($user)->get(route('purchase-history'));
+        $response->assertOk();
+    }
+
+    // user can see wallet balance
+    public function test_that_user_can_see_wallet_balance_and_buttons(){
+
+        $user = factory(User::class)->create();
+        $amount = 100000;
+        $wallet = Wallet::create([
+            'user_id' => $user,
+            'balance' => -$amount,
+            'reference' => 'Nil',
+            'authorization' => 'Nil',
+            'nature_of_tranx' => 'Gift to '.$user->name
+        ]);
+
+        $response = $this->actingAs($user)->get(route('wallet'));
+        $response->assertSeeInOrder(['Fund Wallet', 'Gift Funds', 'History']);
+        $this->assertDatabaseHas('wallets', ['balance' => Wallet::first()->balance]);
+    }
 }
